efaa44e3847c3dc575bb5f6b7885a9f1e92993f3
hookehu <huwen171@126.com>
2013-05-15 15:57:47 +0800

    add tool.py to pack xml to hmf

---
+++ HMF/python/tool.py
@@ -0,0 +1,98 @@
+#-*- coding:utf-8 -*-
+'''
+The MIT License (MIT)
+
+Copyright(C) 2013 <Hooke HU>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+from xml.etree import ElementTree as etree
+from hmf import Hmf
+
+def read_cfg(filename):
+		fd = open(filename, "rb")
+		content = fd.read()
+		fd.close()
+		x = etree.XML(content)
+		rst = {}
+		roots = x.getchildren()
+		for r in roots:
+				children = r.getchildren()
+				if len(children) == 0:
+						continue
+				r = {}
+				for i in children:
+						r[i.tag] = convert_value(i.tag, i.text)
+				if r.has_key('id_i'):
+						rst[r['id_i']] = r
+				else:
+					rst[r['id']] = r
+		return rst
+		
+def convert_value(key, value):
+		subfix = key[-2:]
+		if subfix == "_i":
+				if value == "":
+						return 0
+				return int(value)
+		elif subfix == "_f":
+				if value == "":
+						return 0.0
+				return float(value)
+		elif subfix == "_l":
+				if not isinstance(value, str) and not isinstance(value, unicode):
+						return [int(value)]
+				l = value.split(",")
+				for i in range(len(l)):
+						l[i] = int(l[i])
+				return l
+		elif subfix == "_k":
+				if not isinstance(value, str) and not isinstance(value, unicode):
+						return [int(value)]
+				k = value.split(",")
+				for i in range(len(k)):
+						k[i] = int(k[i])
+				return k
+		elif subfix == "_m":
+				if value == "":
+						return {}
+				m = value.split(",")
+				rv = {}
+				for i in range(len(m)):
+						kv = m[i].split(":")
+						if kv[0].isdigit():
+								k1 = int(kv[0])
+						else:
+								k1 = kv[0]
+						if kv[1].isdigit():
+								v1 = int(kv[1])
+						else:
+								v1 = kv[1]
+						rv[k1] = v1
+				return rv
+		elif subfix == "_s":
+				return value
+		else:
+				return value
+		
+if __name__ == "__main__":
+		import sys, os, dircache
+		base_path = '''E:/mogo/doc/product/配置表/xml文件最终版/'''
+		out_path = '''E:/hmfoutput/'''
+		files = dircache.listdir(base_path)
+		for name in files:
+				if name == '.svn':
+						continue
+				print name
+				r = read_cfg(base_path + name)
+				h = Hmf()
+				h.write_object(r)
+				h.merge_all()
+				s = h.stream
+				s.seek(0)
+				fd = open(out_path + name[:-4] + '.hmf', "wb")
+				fd.write(s.read())
+				fd.close()
